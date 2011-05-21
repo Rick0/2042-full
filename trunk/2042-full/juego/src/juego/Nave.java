1a1b936915b88c1a34b075a7150a8a2d12d8e1cf
hcan27 <hcan27@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-05-21 19:25:37 +0000

    Agregue algunos métodos

--- trunk/2042-full/juego/src/juego/Nave.java
+++ trunk/2042-full/juego/src/juego/Nave.java
@@ -7,5 +7,30 @@ public abstract class Nave extends ObjetoUbicable {
 	boolean destruida;
 	boolean operable;
 	int energia;
+	
+	public void destruirse {
+	"Lleva a cabo las acciones correspondientes si debe destruirse"
+		if ( this.devolverCantidadEnergia>0 ){
+			"NaveNoDestruidaError new signal: 'La nave aun tiene energia en su tanque'"
+		}
+		else {
+			destruida = true;
+			plano.agregarNaveEliminada( this );
+		}
+	}
+	
+	public int devolverCantidadEnergia() {
+	"devuelve la cantidad de energia actual con la que cuenta la nave"
 
+	return energia;
+	
+	}
+	
+	public boolean estadoActualDestruida() {
+	"Devuelve True si la nave está destruida".
+
+	return destruida;
+	}
+	
+	
 }
