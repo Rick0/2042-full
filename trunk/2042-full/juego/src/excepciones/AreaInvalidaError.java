1682b88eb1e4f295a8c8da1063940f4283b15422
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-05-22 22:33:15 +0000

    Agregado AreaInvalidaError

---
+++ trunk/2042-full/juego/src/excepciones/AreaInvalidaError.java
@@ -0,0 +1,16 @@
+package excepciones;
+
+public class AreaInvalidaError extends Exception {
+
+	/**
+	 * Este error deberia lanzarse al intentar mover el algo42
+	 * fuera del area de juego
+	 */
+	private static final long serialVersionUID = -8201025231792397306L;
+	
+	public AreaInvalidaError( String cadena ){
+
+		super( cadena ); // constructor param. de Exception
+
+		}
+}
