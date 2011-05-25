dbf41223b25f60a808a2c7639614399cee71eb16
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-05-25 19:14:29 +0000

    Agregada Clase no operable. Actualizacion anterir modificaciones en nave, algo42, arma, etc. Posiblemente todas las excepciones y errores en arma sean ahora innecesarias, pero no fueron borradas

---
+++ trunk/2042-full/juego/src/juego/NaveNoOperable.java
@@ -0,0 +1,35 @@
+package juego;
+
+import excepciones.NaveNoDestruidaError;
+
+public abstract class NaveNoOperable extends Nave{
+/*Todas las naves que no pueden ser utilizadas por el jugador deben 
+ * heredar de esta clase abstracta.*/
+	
+	boolean fueraDeJuego;
+	boolean puntos;
+	
+	abstract void IntentarAccionSobre(Algo42 algo42);
+	/*La nave se mueve y lleva a cabo su funcion: Si esta en la posicion de algo42 lo choca. Si
+lanza un arma, la agrega a la lista de Armas.*/
+	
+	public void destruirse() throws NaveNoDestruidaError {
+		/*Lleva a cabo las acciones correspondientes si debe destruirse*/
+			if ( (this.devolverCantidadEnergia())>0 ){
+				throw new NaveNoDestruidaError("La nave aun tiene energia en su tanque");
+			}
+			else {
+				destruida = true;
+				plano.agregarNaveEliminada( this );
+			}
+		}
+	
+	public void modificarEnergia(int puntosModificar) throws NaveNoDestruidaError{
+		/*Recibe una cierta cantidad de puntos y los suma a la energÃ­a de la nave.*/
+
+			energia = energia+puntosModificar;
+			if(energia<=0) {
+				this.destruirse();
+			}
+		}
+}
