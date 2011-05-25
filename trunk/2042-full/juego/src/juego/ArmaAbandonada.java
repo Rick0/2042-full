85bbb9bf550daf27a436bf0f8f5d0798ac81fa7e
hcan27 <hcan27@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-05-25 21:52:15 +0000

    Agregado el constructor

--- trunk/2042-full/juego/src/juego/ArmaAbandonada.java
+++ trunk/2042-full/juego/src/juego/ArmaAbandonada.java
@@ -4,6 +4,13 @@ public class ArmaAbandonada extends Item {
 	
 	private int numeroTorpedos, numeroCohetes;
 	
+	public ArmaAbandonada(int posicionX, int posicionY) {
+		usado = false;
+		numeroTorpedos = 2;
+		numeroCohetes = 3;
+		rectangulo = new Rectangulo(3, 3, posicionX, posicionY);
+	}
+	
 	void intentarEfectoEn(Algo42 algo42) {
 		//Recibe una nave tipo Algo42; y le suma las armas que dejo la nave enemiga.
 		if ( algo42.coincidePosicionCon(this.rectangulo) ) {
