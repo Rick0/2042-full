b19b8eb74fb6b6678d96626ccc1d7d9889909007
hcan27 <hcan27@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-05-25 22:08:49 +0000

    Estructura de nave no operable agregada

---
+++ trunk/2042-full/juego/src/juego/Caza.java
@@ -0,0 +1,11 @@
+package juego;
+
+public class Caza extends NaveNoOperable {
+
+	@Override
+	void IntentarAccionSobre(Algo42 algo42) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/2042-full/juego/src/juego/Civil.java
@@ -0,0 +1,11 @@
+package juego;
+
+public class Civil extends NaveNoOperable {
+
+	@Override
+	void IntentarAccionSobre(Algo42 algo42) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
