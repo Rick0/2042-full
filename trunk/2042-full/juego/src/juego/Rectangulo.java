e3b5a54771c50a4997acdbce9f73d2c4abbe2aa5
ricky488@gmail.com <ricky488@gmail.com@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-03 04:29:25 +0000

    Rectangulo indentado

--- trunk/2042-full/juego/src/juego/Rectangulo.java
+++ trunk/2042-full/juego/src/juego/Rectangulo.java
@@ -1,95 +1,90 @@
 package juego;
 
+/* Clase rectangulo, crea figuras rectangulares con un determinado ancho
+ * y altura, ademas de una posicion en el espacio.
+ */
 public class Rectangulo {
-		/*Clase rectangulo, crea figuras rectangulares con un determinado ancho
-		 * y altura, ademas de una posicion en el espacio.
-		 */
-		
-		int ancho;
-		int altura;
-		double posicionX;
-		double posicionY;
-		
-		public Rectangulo(int alturaR, int anchoR, double x, double y ) {
-		/*Constructor del rectangulo, recibe su posicion en el espacio y sus dimensiones*/
-			posicionX=x;
-			posicionY=y;
-			ancho=anchoR;
-			altura=alturaR;
-			
+
+	int ancho;
+	int altura;
+	double posicionX;
+	double posicionY;
+
+
+	/* Constructor del rectangulo, recibe su posicion en el espacio y sus dimensiones */
+	public Rectangulo(int alturaR, int anchoR, double x, double y ) {
+		posicionX=x;
+		posicionY=y;
+		ancho=anchoR;
+		altura=alturaR;
+	}
+
+	/* Devuelve la posicion en Y del rectangulo */
+	public double devolverPosicionY() {
+		return posicionY;
+	}
+
+	/* Devuelve la posicion en X del rectangulo */
+	public double devolverPosicionX() {
+		return posicionX;
+	}
+
+	/* Devuelve la altura del rectangulo */
+	public int devolverAltura() {
+		return altura;	
+	}
+
+	/* Devuelve el ancho del rectangulo */
+	public int devolverAncho() {
+		return ancho;
+	}
+
+	public void determinarPosicion(double posx, double posy) {
+		posicionX = posx;
+		posicionY = posy;
+	}
+
+	/* Compara dos rectangulos y devuelve true si se superponen
+	 * (es decir, si tienen en comun al menos un punto)
+	 * o false en caso contrario.
+	 */
+	public boolean coincideConPosicionDe(Rectangulo otroRectangulo) {
+
+		double otroXComienzo, otroXFinal, XComienzo, XFinal, otroYComienzo, otroYFinal, YComienzo, YFinal;
+	
+		otroXComienzo = otroRectangulo.posicionX;
+		otroXFinal = otroXComienzo + otroRectangulo.ancho;
+		XComienzo = posicionX;
+		XFinal = XComienzo + ancho;
+
+
+		if(XComienzo <= otroXComienzo) {
+			if (!(otroXComienzo<=XFinal)) {
+				return false;
+			}
 		}
-		
-		public double devolverPosicionY() {
-			/*Devuelve la posicion en Y del rectangulo*/
-				return posicionY;
-				
+		if (XComienzo >= otroXComienzo) {
+			if(!(XComienzo<=otroXFinal)) {
+				return false;
 			}
-		
-		public double devolverPosicionX() {
-			/*Devuelve la posicion en X del rectangulo*/
-				return posicionX;
-				
-			}
-		
-		public int devolverAltura() {
-			/*Devuelve la altura del rectangulo*/
-				return altura;
-				
 		}
 
-		public int devolverAncho() {
-			/*Devuelve el ancho del rectangulo*/
-				return ancho;
-				
+		otroYComienzo = otroRectangulo.posicionY;
+		otroYFinal = otroYComienzo + otroRectangulo.altura;
+		YComienzo = posicionY;
+		YFinal = YComienzo + altura;
+		if(YComienzo <= otroYComienzo) {
+			if (!(otroYComienzo<=YFinal)) {
+				return false;
+			}
 		}
-		
-		public void determinarPosicion(double posx, double posy){
-		
-			posicionX = posx;
-			posicionY = posy;
+		if (YComienzo >= otroYComienzo) {
+			if (!(YComienzo<=otroYFinal)) {
+				return false;
+			}
 		}
-		
-		public boolean coincideConPosicionDe(Rectangulo otroRectangulo){
-			
-			/*Compara dos rectangulos y devuelve true si se superponen
-			 * (Es decir, si tienen en comun al menos un punto) o false en caso contrario.
-			 */
-			
-			double otroXComienzo, otroXFinal, XComienzo, XFinal, otroYComienzo, otroYFinal, YComienzo, YFinal;
-		
-			otroXComienzo = otroRectangulo.posicionX;
-			otroXFinal = otroXComienzo + otroRectangulo.ancho;
-			XComienzo = posicionX;
-			XFinal = XComienzo + ancho;
-		
-		
-			if(XComienzo <= otroXComienzo){
-				if (!(otroXComienzo<=XFinal)){
-					return false;
-				}
-			}
-			if (XComienzo >= otroXComienzo){
-				if(!(XComienzo<=otroXFinal)){
-					return false;
-				}
-			}
-		
-			otroYComienzo = otroRectangulo.posicionY;
-			otroYFinal = otroYComienzo + otroRectangulo.altura;
-			YComienzo = posicionY;
-			YFinal = YComienzo + altura;
-			if(YComienzo <= otroYComienzo){
-				if (!(otroYComienzo<=YFinal)){
-					return false;
-				}
-			}
-			if (YComienzo >= otroYComienzo){
-				if (!(YComienzo<=otroYFinal)){
-					return false;
-				}
-			}
-			return true;
-	
-			}
-		
+		return true;
+
+	}
+
 }
