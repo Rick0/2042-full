8404ad59f4f046c56adaea78c2c7b03a394290d1
ricky488@gmail.com <ricky488@gmail.com@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-03 04:43:25 +0000

    ObjetoUbicable y todos los Items indentado

--- trunk/2042-full/juego/src/juego/ArmaAbandonada.java
+++ trunk/2042-full/juego/src/juego/ArmaAbandonada.java
@@ -1,23 +1,25 @@
 package juego;
 
 public class ArmaAbandonada extends Item {
-	
+
 	private int numeroTorpedos, numeroCohetes;
-	
+
+
 	public ArmaAbandonada(double d, double e) {
 		usado = false;
 		numeroTorpedos = 2;
 		numeroCohetes = 3;
 		rectangulo = new Rectangulo(3, 3, d, e);
 	}
-	
+
+	/* Recibe una nave tipo Algo42; y le suma las armas que dejo la nave enemiga */
 	public void intentarEfectoEn(Algo42 algo42) {
-		//Recibe una nave tipo Algo42; y le suma las armas que dejo la nave enemiga.
+
 		if ( algo42.coincidePosicionCon(this.rectangulo) ) {
 			algo42.aumentarArmas( this.numeroTorpedos, this.numeroCohetes );
 			this.usado = true;
 		}
-		
+
 	}
 
 }
--- trunk/2042-full/juego/src/juego/ObjetoUbicable.java
+++ trunk/2042-full/juego/src/juego/ObjetoUbicable.java
@@ -1,44 +1,44 @@
 package juego;
 
+/* Todos los objetos que se pueden ubicar en el escenario de juego 
+ * heredan de esta clase: Todos deben tener una instancia de rectangulo
+ * que los representa en el espacio y que les da un tamanio y una ubicacion
+ */
 public abstract class ObjetoUbicable {
-	
-	/*Todos los objetos que se pueden ubicar en el escenario de juego 
-	 * heredan de esta clase: Todos deben tener una instancia de rectangulo
-	 * que los representa en el espacio y que les da un tamanioo 
-	 * y una ubicacion.*/
-	
+
 	Rectangulo rectangulo;
 	Plano plano;
-	
-	public void determinarPosicion(double posx,double posy){
-		/*Ubica el objeto en la posicion determinada*/
+
+
+	/* Ubica el objeto en la posicion determinada */
+	public void determinarPosicion(double posx,double posy) {
 		rectangulo.determinarPosicion(posx,posy);
 	}
 
-	public double posicionX(){
-		/*Devuelve la posicion en X del objeto*/
+	/* Devuelve la posicion en X del objeto */
+	public double posicionX() {
 		return rectangulo.devolverPosicionX();
 	}
-	
-	public double posicionY(){
-		/*Devuelve la posicion en Y del objeto*/
+
+	/* Devuelve la posicion en Y del objeto */
+	public double posicionY() {
 		return rectangulo.devolverPosicionY();
 	}
-	
-	public boolean coincidePosicionCon( Rectangulo figuraRectangulo) {
-		
-	/*Toda instancia de esta clase contiene un rectangulo que representa su figura 
-	Este metodo devuelve true si la posicion del objeto ubicable se superpone en 
-	al menos un punto con el rectangulo que recibe por parametro.*/
-	
-	return ( this.rectangulo.coincideConPosicionDe( figuraRectangulo ));
-	}
-	
-	public void determinarPlano(Plano planoDelObjeto) {
-		/*Toda instancia de objeto ubicable esta contenida en un plano. Para permitir la interaccion entre objetos
-		contenidos en el mismo plano, toda instancia de ObjetoUbicable tiene una referencia al plano que lo contiene.
-		Este metodo sirve para determinar el plano del objeto.*/
 
+	/* Toda instancia de esta clase contiene un rectangulo que representa su figura 
+	 * Este metodo devuelve true si la posicion del objeto ubicable se superpone en 
+	 * al menos un punto con el rectangulo que recibe por parametro
+	 */
+	public boolean coincidePosicionCon( Rectangulo figuraRectangulo) {
+		return (this.rectangulo.coincideConPosicionDe( figuraRectangulo ));
+	}
+
+	/* Toda instancia de objeto ubicable esta contenida en un plano. Para permitir la interaccion entre objetos
+	 * contenidos en el mismo plano, toda instancia de ObjetoUbicable tiene una referencia al plano que lo contiene.
+	 * Este metodo sirve para determinar el plano del objeto
+	 */
+	public void determinarPlano(Plano planoDelObjeto) {
 		this.plano = planoDelObjeto;
 	}
+
 }
