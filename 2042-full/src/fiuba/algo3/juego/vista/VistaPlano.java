35153ac9daf4bfa76b390f6770841d13c027d7ab
ricky488@gmail.com <ricky488@gmail.com@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-17 03:56:40 +0000

    Ahora se ve el Algo42 como AvionCool.jpg

--- 2042-full/src/fiuba/algo3/juego/vista/VistaPlano.java
+++ 2042-full/src/fiuba/algo3/juego/vista/VistaPlano.java
@@ -3,10 +3,12 @@ package fiuba.algo3.juego.vista;
 import java.awt.Color;
 import fiuba.algo3.titiritero.vista.Cuadrado;
 
+
 public class VistaPlano extends Cuadrado{
+
 	public VistaPlano() {
 		super(500,500);
-		setColor(Color.BLACK);
+		setColor(Color.WHITE);
 	}
-	
+
 }
