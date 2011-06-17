827ef475229d1591569be71787c56441d54aacb2
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-17 18:37:13 +0000

    Modificado para que la nave se vea con transparencia.

--- 2042-full/src/fiuba/algo3/juego/vista/VistaAlgo42.java
+++ 2042-full/src/fiuba/algo3/juego/vista/VistaAlgo42.java
@@ -6,8 +6,7 @@ import fiuba.algo3.titiritero.vista.Imagen;
 public class VistaAlgo42 extends Imagen {
 
 	public VistaAlgo42() {
-		this.setNombreArchivoImagen("recursos/Nave/AvionCool.jpg");
-	//	this.setNombreArchivoImagen("/fiuba/algo3/juego/vista/recursos/Nave/AvionCool.jpg");
+		this.setNombreArchivoImagen("recursos/Nave/AvionCool.png");
 	}
 
 }
