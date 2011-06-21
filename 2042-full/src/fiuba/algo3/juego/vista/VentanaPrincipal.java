6819aa8ff4a9c185a3408f07fd1b7c4eef656623
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-21 19:31:44 +0000

    Armando el keylistener del algo42

--- 2042-full/src/fiuba/algo3/juego/vista/VentanaPrincipal.java
+++ 2042-full/src/fiuba/algo3/juego/vista/VentanaPrincipal.java
@@ -5,13 +5,9 @@ import java.awt.Color;
 import java.awt.Event;
 import java.awt.Frame;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ConcurrentModificationException;
-
 import fiuba.algo3.juego.modelo.Algo42;
-import fiuba.algo3.juego.modelo.excepciones.AreaInvalidaError;
 import fiuba.algo3.juego.controlador.*;
 import fiuba.algo3.titiritero.SuperficieDeDibujo;
 
@@ -31,74 +27,11 @@ public class VentanaPrincipal extends Frame {
 		this.setResizable(false);
 		panel = new Panel(550, 570,controladorJuego);
 		this.add(panel,BorderLayout.CENTER);
-		
 		this.setBackground(Color.black);
 		panel.setBackground(Color.black);
 
-
-		this.addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyPressed(KeyEvent arg0) {
-				int opcion=arg0.getKeyCode();
-				if(opcion==KeyEvent.VK_DOWN){
-					//Abajo
-					try {
-						algo.moverAbajo();
-					} catch (AreaInvalidaError e) {
-						System.out.print("Area invalida");
-						// Si hay error de area invalida, que no se mueva.
-					}
-				}
-				if(opcion==KeyEvent.VK_UP){
-				//Arriba
-					try {
-						algo.moverArriba();
-					} catch (AreaInvalidaError e) {
-						System.out.print("Area invalida");
-						// Si hay error de area invalida, que no se mueva.
-					}
-				}
-				if(opcion==KeyEvent.VK_LEFT){
-					//izquierda
-					try {
-						algo.moverIzquierda();
-					} catch (AreaInvalidaError e) {
-						System.out.print("Area invalida");
-						// Si hay error de area invalida, que no se mueva.
-					}
-				}
-				if(opcion==KeyEvent.VK_RIGHT){
-					//Derecha
-					try {
-						algo.moverDerecha();
-					} catch (AreaInvalidaError e) {
-						System.out.print("Area invalida");
-						// Si hay error de area invalida, que no se mueva.
-					}
-				}
-				if(opcion==KeyEvent.VK_SPACE){
-					//Disparo
-					try{
-						algo.dispararLaser();
-					}
-					catch(ConcurrentModificationException e){
-						algo.dispararLaser();
-					}
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-			}
-		});
-
+		this.addKeyListener(new KeyListenerAlgo42(algo));
+		
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				dispose();
