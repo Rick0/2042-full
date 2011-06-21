b57d2f2d0475a44c4140f8f6f7301ed425a0492f
ricky488@gmail.com <ricky488@gmail.com@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-21 22:08:04 +0000

    Agregue \n atras de los mensajes de error, dps deberiamos ponerlos en todos los mensajes de error.

--- 2042-full/src/fiuba/algo3/juego/controlador/operacionesAlgo/FlechaDerechaControlable.java
+++ 2042-full/src/fiuba/algo3/juego/controlador/operacionesAlgo/FlechaDerechaControlable.java
@@ -3,23 +3,25 @@ package fiuba.algo3.juego.controlador.operacionesAlgo;
 import fiuba.algo3.juego.modelo.Algo42;
 import fiuba.algo3.juego.modelo.excepciones.AreaInvalidaError;
 
+
 public class FlechaDerechaControlable implements Controlable{
 
 	Algo42 algo42;
-	
+
+
 	public FlechaDerechaControlable(Algo42 algo) {
 		algo42=algo;
 	}
 
-	
 	/*El algo42 se mueve a la derecha*/
 	@Override
 	public void activarEfecto() {
 		try {
 			algo42.moverDerecha();
 		} catch (AreaInvalidaError e) {
-			System.out.print("Area invalida");
+			System.out.print("Area invalida\n");
 			// Si hay error de area invalida, que no se mueva.
 		}		
 	}
+
 }
