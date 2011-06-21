3225f2e2f71b443b873217f79be6fa6dd3f8f869
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-21 18:57:21 +0000

    Agregada la vista de la pantalla principal y la imagen

---
+++ 2042-full/src/fiuba/algo3/juego/vista/VistaPantallaPrincipal.java
@@ -0,0 +1,12 @@
+package fiuba.algo3.juego.vista;
+
+import fiuba.algo3.titiritero.vista.Imagen;
+
+
+public class VistaPantallaPrincipal extends Imagen {
+
+	public VistaPantallaPrincipal() {
+		this.setNombreArchivoImagen("recursos/PantallaPrincipal/pantallaPrincipal.png");
+	}
+
+}
