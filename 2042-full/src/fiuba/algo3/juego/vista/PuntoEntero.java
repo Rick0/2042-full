066a6f480bc2c832be641a6e743566ad202d689d
hcan27 <hcan27@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-06-23 04:50:16 +0000

    Punto entero posicionable, sirve para cosas estaticas

---
+++ 2042-full/src/fiuba/algo3/juego/vista/PuntoEntero.java
@@ -0,0 +1,32 @@
+package fiuba.algo3.juego.vista;
+
+import fiuba.algo3.titiritero.Posicionable;
+
+public class PuntoEntero implements Posicionable{
+
+	public PuntoEntero(int x, int y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	@Override
+	public int getX() {
+		return X;
+	}
+
+	@Override
+	public int getY() {
+		return Y;
+	}
+	
+	public void setX(int x) {
+		X = x;
+	}
+
+	public void setY(int y) {
+		this.Y = y;
+	}
+
+	private int X;
+	private int Y;
+}
