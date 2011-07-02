c25f3bf553fbe78ca6ae200cf61801f98b1bfbd0
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-07-02 07:16:06 +0000

    Mas modificaciones en main

--- trunk/2042-full/src/fiuba/algo3/juego/programa/Programa.java
+++ trunk/2042-full/src/fiuba/algo3/juego/programa/Programa.java
@@ -1,8 +1,5 @@
 package fiuba.algo3.juego.programa;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
 public class Programa {
 
 	/**
@@ -10,13 +7,7 @@ public class Programa {
 	 * Inicializa el juego.
 	 */
 	public static void main(String[] args) {
-		
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				VentanaInicial v= new VentanaInicial();
-				v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				v.setVisible(true);
-			}
-		});
+		EjecutableJuego e= new EjecutableJuego();
+		e.run();
 	}
 }
