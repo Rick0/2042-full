85475f9fa86cde5c0b18436d1b31ccdb7ed1516b
aguila.cudicio <aguila.cudicio@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-07-02 05:36:07 +0000

    Modificacion main y cambio en armas para que:
    * Se muevan más rapido (Movimiento general en la clase arma, no cuenta para las que redefinen mover)
    * Algo dispare mas rapido.

--- trunk/2042-full/src/fiuba/algo3/juego/programa/Programa.java
+++ trunk/2042-full/src/fiuba/algo3/juego/programa/Programa.java
@@ -1,5 +1,10 @@
 package fiuba.algo3.juego.programa;
 
+import fiuba.algo3.juego.controlador.ControladorJuegoAlgo42full;
+import fiuba.algo3.juego.controlador.GeneradorControlador;
+import fiuba.algo3.juego.controlador.GeneradorFlota;
+import fiuba.algo3.juego.modelo.Plano;
+
 public class Programa {
 
 	/**
@@ -8,14 +13,14 @@ public class Programa {
 	 */
 	public static void main(String[] args) {
 
-		/*Plano plano = new Plano(550,570);
+		Plano plano = new Plano(550,570);
 		GeneradorControlador generador = new GeneradorControlador(plano);
 		ControladorJuegoAlgo42full controlador = generador.generarControlador();
 
 		GeneradorFlota generadorFlota = new GeneradorFlota(plano);
 		controlador.agregarObjetoVivo(generadorFlota);
 
-		controlador.comenzarJuego();*/
+		controlador.comenzarJuego();
 		new VentanaInicial();
 	}
 
