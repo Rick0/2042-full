f9650977cfd29a253dfd123719ac871dc31827e1
hcan27 <hcan27@404fb3e1-2a34-4d82-4f61-5b30a1275364>
2011-07-04 17:00:22 +0000

    Se agregaron textos de visibilidad.

--- trunk/2042-full/src/fiuba/algo3/juego/controlador/operacionesAlgo/BotonXControlable.java
+++ trunk/2042-full/src/fiuba/algo3/juego/controlador/operacionesAlgo/BotonXControlable.java
@@ -30,12 +30,14 @@ public class BotonXControlable implements Controlable {
 		if (listaNavesEnemigas.size() > 0) {
 			try {
 				algo.dispararTorpedoRastreadorHacia(listaNavesEnemigas.get(0));
+				System.out.println("Disparar Torpedo Rastreador()");
 			}
 			catch (ArmaNoDisponibleError e) {
-				// Si no tiene el arma, que no haga nada.
+				System.out.println("No tiene torpedos");
 			}
 			catch (NaveARastrearError e) {
 				// Problema con la nave a rastrear
+				System.out.println("No se puede rastrear esa nave");
 			}
 		}
 	}
